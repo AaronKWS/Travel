a3b2ba23c1a878b2d9ee434c6a339ef8ce61ea9a
AaronKWS <43720281+AaronKWS@users.noreply.github.com>
2018-10-26 00:16:47 +0800

    Header的制作

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -4,8 +4,9 @@ import router from './router'
 import store from './store'
 import fastClick from 'fastclick'       //防止点击延迟
 import './registerServiceWorker'
-import './assets/styles/reset.css'      //页面样式初始化
-import './assets/styles/border.css'     //防止移动端像素异常
+import 'styles/reset.css'      //页面样式初始化
+import 'styles/border.css'     //防止移动端像素异常
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
