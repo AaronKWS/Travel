922893290010d64fbc27d2b9b14b3d7c67025861
AaronKWS <43720281+AaronKWS@users.noreply.github.com>
2018-10-27 22:06:37 +0800

    加入推荐栏

---
+++ my-project/src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <div class="title"><span class="iconfont love-icon">&#xe60c;</span><p class="title-desc">猜你喜欢</p></div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src="item.imgUrl">
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div> 
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1503/33/33f6ddd58bf9811.water.jpg_200x200_8fc46d88.jpg",
+          title: "响沙湾",
+          desc: "内蒙古鄂尔多斯市达拉特旗响沙湾"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1710/36/3626151932799e9aa3.img.jpg_200x200_340c950e.jpg",
+          title: "万家惠欢乐世界",
+          desc: "内蒙古鄂尔多斯市达拉特旗响沙湾"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1705/c6/c6d282a696ad2836a3.img.jpg_200x200_fff0128f.jpg",
+          title: "夜鸣沙旅游区",
+          desc: "内蒙古鄂尔多斯市达拉特旗响沙湾"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+.title 
+    margin-top: 0.1rem;
+    line-height: 0.8rem;
+    background-color: #ccc;
+    text-indent: 0.2rem;
+    .love-icon 
+        display: block;
+        float: left;
+        color: rgb(255, 91, 88);
+        font-size: 20px;
+    .title-desc
+        font-size 16px
+        font-family Arial,Verdana,Sans-serif
+.item
+    overflow: hidden;
+    display: flex;
+    height: 1.9rem;
+    .item-img 
+        width: 1.7rem;
+        height: 1.7rem;
+        padding: 0.1rem;
+    .item-info 
+        flex: 1;
+        padding: 0.1rem;
+        min-width: 0;
+        .item-title 
+            line-height: 0.54rem;
+            font-size: 0.32rem;
+            ellipsis();
+        .item-desc 
+            line-height: 0.4rem;
+            color: #ccc;
+            ellipsis();
+        .item-button
+            line-height: 0.44rem;
+            margin-top: 0.16rem;
+            background-color: #ff9300;
+            padding: 0 0.2rem;
+            border-radius: 0.06rem;
+            color: #fff;
+</style>
---
+++ my-project/src/pages/home/components/Weekend.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="where">
+        <div class="title"><p class="title-desc">周末去哪儿</p></div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div> 
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1711/a1/6d51d5059274da.jpg_r_640x214_34ec6f3e.jpg",
+          title: "内蒙古泡汤圣地",
+          desc: "内蒙古冬季不再冷，来泡温泉吧"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1606/a2/c10424cf4acf79.jpg_r_640x214_d83730b5.jpg",
+          title: "呼市必游TOP5",
+          desc: "蒙古族歌手高亢，使人回味悠长"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1511/14/1cf5dc2ecbfc16.jpg_r_640x214_b4b3a58a.jpg",
+          title: "最美丝路行",
+          desc: "情系西北，中国梦丝路行"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.where
+    margin-top .2rem
+    .title 
+        margin-top: 0.1rem
+        line-height: 0.8rem
+        background-color: #ccc
+        text-indent: 0.2rem
+        .title-desc
+            font-size 16px
+            font-family Arial,Verdana,Sans-serif
+    .item-img-wrapper
+        overflow hidden
+        height 0
+        padding-bottom 33.9%
+        .item-img 
+            width: 100%
+    .item-info 
+        padding: .1rem
+        .item-title 
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc 
+            line-height: 0.4rem
+            color: #ccc
+            ellipsis()
+</style>
