93ac03f59c6fc6a55a0570659b8b6a7b15428e1e
AaronKWS <43720281+AaronKWS@users.noreply.github.com>
2018-10-29 18:14:27 +0800

    Revert "Revert "Merge branch 'index-ajax'""
    This reverts commit 6b3eb42bef450f0ca0491f139e64395e9d5ced7c.

--- my-project/vue.config.js
+++ my-project/vue.config.js
@@ -4,6 +4,16 @@ function resolve(dir) {
     return path.join(__dirname, dir)
 }
 module.exports = {
+    devServer: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8080',
+            pathRewrite: {
+                '^/api': '/mock'
+            }
+          }
+        }
+    },
     lintOnSave: true,
     chainWebpack: (config) => {
         config.resolve.alias
