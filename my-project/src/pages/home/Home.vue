e6ce6294a4653239a4843bf484b1a5c6208a9a5e
AaronKWS <43720281+AaronKWS@users.noreply.github.com>
2018-11-05 23:54:57 +0800

    使用keep-alive优化网页性能

--- my-project/src/pages/home/Home.vue
+++ my-project/src/pages/home/Home.vue
@@ -15,6 +15,8 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
+
 export default{
     name: 'Home',
     components: {
@@ -26,15 +28,19 @@ export default{
     },
     data () {
         return {
+            lastCity: '',
             swiperList: [],
             iconsList: [],
             recommendList: [],
             weekendList: []
         }
     },
+    computed: {
+        ...mapState(['city'])
+    },
     methods: {
         getHomeInfo () {
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city=' + this.city)
                 .then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc (res) {
@@ -49,8 +55,15 @@ export default{
         }
     },
     mounted() {
+        this.lastCity = this.city
         this.getHomeInfo()
     },
+    activated() {               //使用keep - alive会多出这个生命周期函数
+        if(this.lastCity !== this.city){
+            this.lastCity = this.city
+            this.getHomeInfo()
+        }
+    }
 }
 </script>
 
